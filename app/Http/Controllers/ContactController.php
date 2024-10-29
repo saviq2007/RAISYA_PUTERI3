7e1954ad6085d800717a5390c667b22207ec008f
Ahmad Saviq Firda <saviq075@gmail.com>
2024-10-29 12:02:46 +0700

    first commit

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Contact;
+use Illuminate\Http\Request;
+
+class ContactController extends Controller
+{
+    public function index()
+    {
+        $contacts = Contact::all();
+        return view('contacts.index', compact('contacts'));
+    }
+
+    public function create()
+    {
+        return view('contacts.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'nullable|email',
+            'phone' => 'nullable'
+        ]);
+        Contact::create($request->all());
+        return redirect()->route('contacts.index');
+    }
+
+    public function edit(Contact $contact)
+    {
+        return view('contacts.edit', compact('contact'));
+    }
+
+    public function update(Request $request, Contact $contact)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'nullable|email',
+            'phone' => 'nullable'
+        ]);
+        $contact->update($request->all());
+        return redirect()->route('contacts.index');
+    }
+
+    public function destroy(Contact $contact)
+    {
+        $contact->delete();
+        return redirect()->route('contacts.index');
+    }
+}
+
